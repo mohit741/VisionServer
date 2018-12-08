c4e6a40d33022e388b3761b9b26490f75e81af76
mohit741 <mohit.verma741@gmail.com>
2018-12-08 14:11:17 +0530

    Added image upload code

--- VisionServer.py
+++ VisionServer.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+file_handler = logging.FileHandler('server.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = '{}/uploads/'.format(PROJECT_HOME)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -8,5 +18,20 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    app.logger.info(PROJECT_HOME)
+    if request.method == 'POST' and request.files['image']:
+        app.logger.info(app.config['UPLOAD_FOLDER'])
+        img = request.files['image']
+        img_name = secure_filename(img.filename)
+        saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+        app.logger.info("saving {}".format(saved_path))
+        img.save(saved_path)
+        return 200
+    else:
+        return 404
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='apivision.azurewebsites.net', debug=True)
